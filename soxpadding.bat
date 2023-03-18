08a5f892d5ac990236d89e9cb7f0825f8d386a51
guiltlab <45518224+guiltlab@users.noreply.github.com>
2023-03-18 01:25:55 +0100

    update script to work on whole folder

--- soxpadding.bat
+++ soxpadding.bat
@@ -1,5 +1,14 @@
-:: this script exectues sox -n stats on every file selected in foobar2000, by using the Run Service option
+:: this script exectues sox -n stats on every FLAC present in a folder selected in foobar2000, by using the Run Service option
 :: this is useful to detect bit padding, if a file is really 24 bit it will show 24/24, if it's padded with zeroes it will show 16/16
 :: Run Service Path: "C:\path\soxpadding.bat" "%path%"
-sox %1 -n stats
+
+@echo off
+
+set "folder_path=%~dp1"
+
+for %%F in ("%folder_path%\*.flac") do (
+	echo Processing file: "%%~nxF"
+    sox "%%F" -n stats 2>&1 | findstr /C:"Bit-depth" /C:"Overall"
+)
+
 pause
