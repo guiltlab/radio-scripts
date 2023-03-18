b052403175c1b6c7f8e2664d23b02338fa5fa0b4
guiltlab <45518224+guiltlab@users.noreply.github.com>
2023-03-18 13:41:19 +0100

    unicode limitation comment

--- soxpadding.bat
+++ soxpadding.bat
@@ -1,6 +1,7 @@
 :: this script exectues sox -n stats on every FLAC present in a folder selected in foobar2000, by using the Run Service option
 :: this is useful to detect bit padding, if a file is really 24 bit it will show 24/24, if it's padded with zeroes it will show 16/16
 :: Run Service Path: "C:\path\soxpadding.bat" "%path%"
+:: this does NOT work on files that have unicode characters in their path
 
 @echo off
 
